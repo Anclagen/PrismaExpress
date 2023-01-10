7e3b3931c5628416dde4047e316b1d3893e12f07
Anclagen <91462616+Anclagen@users.noreply.github.com>
2023-01-10 20:37:57 +0100

    added body parsed an a simple post user

--- src/app.js
+++ src/app.js
@@ -4,11 +4,17 @@ import { applicationRouter } from "./modules/applications/routes.js";
 import { listingsRouter } from "./modules/listings/routes.js";
 import { offersRouter } from "./modules/offers/routes.js";
 import * as dotenv from "dotenv";
+import bodyParser from "body-parser";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(
+  bodyParser.json({
+    extended: false,
+  })
+);
 app.use("/users", userRouter);
 app.use("/applications", applicationRouter);
 app.use("/listings", listingsRouter);
--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -8,3 +8,24 @@ userRouter.get("/", async (req, res) => {
   const users = await prisma.user.findMany();
   res.json(users);
 });
+
+userRouter.post("/", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { firstName, lastName, email, password } = req.body;
+    console.log(firstName);
+    const result = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password,
+      },
+    });
+
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+    res.json({ message: "oops" });
+  }
+});
